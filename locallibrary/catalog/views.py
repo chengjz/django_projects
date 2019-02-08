8ee170e41f114321759631bb110b7312d3a565b1
chengjz <jzcheng@umich.edu>
2019-02-08 19:47:44 +0000

    Detail tutorial complete

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
-
 from catalog.models import Book, Author, BookInstance, Genre
 
 def index(request):
@@ -26,3 +25,42 @@ def index(request):
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
+
+from django.views import generic
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 5
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 5
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+# from django.views import generic
+# from django.shortcuts import get_object_or_404
+
+# class BookListView(generic.ListView):
+#     model = Book
+#     context_object_name = 'my_book_list'   # your own name for the list as a template variable
+#     queryset = Book.objects.filter(title__icontains='war')[:5] # Get 5 books containing the title war
+#     template_name = 'books/my_arbitrary_template_name_list.html'  # Specify your own template name/location
+
+#     def get_queryset(self):
+#         return Book.objects.filter(title__icontains='war')[:5] # Get 5 books containing the title war
+
+#     def get_context_data(self, **kwargs):
+#         # Call the base implementation first to get the context
+#         context = super(BookListView, self).get_context_data(**kwargs)
+#         # Create any data and add it to the context
+#         context['some_data'] = 'This is just some data'
+#         return context
+
+# class BookDetailView(generic.DetailView):
+#     model = Book
+
+#     def book_detail_view(request, primary_key):
+#         book = get_object_or_404(Book, pk=primary_key)
+#         return render(request, 'catalog/book_detail.html', context={'book': book})
